e32bd35129122cff6d3e10dd81721aeda73c5d43
Tristan <tristanlangford@hotmail.com>
2020-04-27 15:41:02 +0000

    included comments

--- directory.rb
+++ directory.rb
@@ -12,8 +12,8 @@ students = [
 {name: "Norman Bates", cohort: :november}
 ]
 
-def month_check(month)
-months_array = [
+def month_check(month) # method to check month input correct
+months_array = [ # array of month
 "january",
 "february",
 "march",
@@ -27,51 +27,51 @@ months_array = [
 "november",
 "december"
 ]
-  loop do
-    if months_array.include?(month.downcase)
-      return month.capitalize
+  loop do # loop until input correct
+    if months_array.include?(month.downcase) # check month in array & downcase input
+      return month.capitalize # return month capitalized
     else
-      puts "ERROR: Please enter a month"
-      month = gets.chomp
+      puts "ERROR: Please enter a month" # error message
+      month = gets.chomp # updates month with new inpu
     end
   end
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit enter twices"
-  students = []
-  name = gets.chomp
-  while name.empty? == false
-    puts "Enter #{name}'s cohort"
-    cohort_month = gets.chomp
-    month = month_check(cohort_month)
-    students.push({name: name, cohort: month})
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+def input_students # Method to request student details
+  puts "Please enter the names of the students" # The request for name
+  puts "To finish, just hit enter twices" # How to exit input method
+  students = [] # Create array to capture details
+  name = gets.chomp # capture name input
+  while name.empty? == false # check to see if use wants to exit
+    puts "Enter #{name}'s cohort" # Request for cohort
+    cohort_month = gets.chomp # user cohort input
+    month = month_check(cohort_month) # call to month check method
+    students.push({name: name, cohort: month}) # push values into array with keys
+    puts "Now we have #{students.count} students" # count check for user
+    name = gets.chomp # next input
   end
-  return students
+  return students # return array of students
 end
 
-def print_header
+def print_header # header for final output
   puts "The students of Villain Academy"
-  puts "----------"
+  puts "----------" # space for asthetics
 end
 
-def print(names)
-  list_num = 1
+def print(names) # method to print names in list
+  list_num = 1 # variable for list index
   names.each { |students| 
     puts "#{list_num}. #{students[:name]} (#{students[:cohort]} cohort)"  
-    list_num += 1
+    list_num += 1 # iterate through list and print line above & update index
   }
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+def print_footer(names) # footer for final output
+  puts "Overall we have #{names.count} great students" # tells user count of students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = input_students # start by requesting users
+print_header # print header
+print(students) # print list of students
+print_footer(students) # print footer
 
